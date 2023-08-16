1f975c8838d39f5b61c074b9f3513dd4b1a9bd21
Isaac-Lee <isaac.lee.3412@gmail.com>
2023-08-16 11:08:44 +0900

    [feat] add query param

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(name: str = None):
+    if name:
+        return {"message": f"Hello World and Hello {name}"}
+    return {"message": f"Hello World"}
 
