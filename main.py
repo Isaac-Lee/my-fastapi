1d19f8301bf0c3a67ed40b49c80309a1ef496c70
Yesung(Isaac) Lee <isaac.lee.3412@gmail.com>
2023-08-26 15:43:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ async def root(name: str = None):
 @app.get("/health")
 async def health():
     return {"message": "health"}
+
+@app.get("/name1")
+async def name1():
+    pass
+
+@app.get("/name2")
+async def name2():
+    pass
